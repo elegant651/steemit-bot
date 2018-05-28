9c737b422ef6f4f5cbb5e1be1a151f9badc1dc7b
elegant651 <korea651@gmail.com>
2018-05-28 16:16:14 +0900

    accepting with dialogflow json

--- dialogflow.js
+++ dialogflow.js
@@ -9,6 +9,75 @@ module.exports = function(app, key) {
 		res.json({ 'speech': msg, 'displayText': msg });
 	}
 
+	const actionOnViewIntent = (discuss_option, res) => {
+		const st_query = {"tag": "kr", "limit": 10};
+ 		if(discuss_option=='최신'){
+ 			app.getDiscussionsByCreated(st_query).then((result) => {
+					let msg = "";
+					result.forEach((entry) => {
+						const url = "https://steemit.com/"+entry.url;
+						msg += entry.title +"\n URL: "+url;
+					});
+					
+					res.json({ 'speech': msg, 'displayText': msg });
+ 			}).catch((err) => {
+ 				notUnderstood(res);
+ 			});
+ 		} else if(discuss_option=='핫한'){	     			
+ 			app.getDiscussionsByHot(st_query).then((result) => {	     				
+					let msg = "";
+					result.forEach((entry) => {
+						const url = "https://steemit.com/"+entry.url;
+						msg += entry.title +"\n URL: "+url;
+					});
+					
+					res.json({ 'speech': msg, 'displayText': msg });	     					     		
+ 			}).catch((err) => {
+ 				notUnderstood(res);
+ 			});	
+ 		}
+	}
+
+	const analyzeForIntent = (body, res) => {		
+		const intentName = body.queryResult.intent.displayName;	
+
+		if(intentName=='view_intent'){	     		
+			const discuss_option = body.queryResult.parameters.discuss_option;
+
+     		actionOnViewIntent(discuss_option, res);
+		} else {
+     	  console.log(`  No intent matched.`);
+	      notUnderstood(res);
+     	} 			
+	}
+
+	const analyzeForIntentV1 = (body, res) => {
+		const objResult = JSON.parse(body);     	
+     	const intentName = objResult.result.metadata.intentName;
+
+     	if(intentName=='view_intent'){	     		
+     		const discuss_option = objResult.result.parameters.discuss_option;
+
+     		actionOnViewIntent(discuss_option, res);
+     	} else {
+     	  console.log(`  No intent matched.`);
+	      notUnderstood(res);
+     	}
+	}
+
+	app.post('/processForDl', (req, res) => {
+		const body = req.body;	
+
+		console.log(JSON.stringify(body));
+
+		if(!body){
+		  notUnderstood(res);
+	  	  return;
+	  	}
+
+		analyzeForIntent(body, res);
+	});
+
 	app.post('/process', (req, res) => {
 	  	const query = req.body.query;
 
@@ -22,52 +91,15 @@ module.exports = function(app, key) {
 	  	const api_url = `https://api.dialogflow.com/v1/query?v=20170712&lang=${language}&sessionId=${session_id}&query=${query}`;
 
 		const options = {
-		  url: encodeURI(api_url),		    
+		  url: encodeURI(api_url),  
 		  headers: {
 		    'Authorization': "Bearer "+TOKEN,
 		    'Content-Type': 'application/x-www-form-urlencoded'
 		  }
 	    };
 	    request.get(options, function (error, response, body) {
-	      if (!error) {			     		     	      	
-	        const objResult = JSON.parse(body);
-	     	// res.json({flag: 1, data: objResult.result.parameters});
-	     	const intentName = objResult.result.metadata.intentName;
-
-	     	if(intentName=='view_intent'){	     		
-	     		const discuss_option = objResult.result.parameters.discuss_option;
-
-	     		const st_query = {"tag": "kr", "limit": 10};
-	     		if(discuss_option=='최신'){
-	     			app.getDiscussionsByCreated(st_query).then((result) => {
-     					let msg = "";
-     					result.forEach((entry) => {
-     						const url = "https://steemit.com/"+entry.url;
-     						msg += entry.title +"\n URL: "+url;
-     					});
-     					
-     					res.json({ 'speech': msg, 'displayText': msg });
-	     			}).catch((err) => {
-	     				notUnderstood(res);
-	     			});
-	     		} else if(discuss_option=='핫한'){	     			
-	     			app.getDiscussionsByHot(st_query).then((result) => {	     				
-     					let msg = "";
-     					result.forEach((entry) => {
-     						const url = "https://steemit.com/"+entry.url;
-     						msg += entry.title +"\n URL: "+url;
-     					});
-     					
-     					res.json({ 'speech': msg, 'displayText': msg });	     					     		
-	     			}).catch((err) => {
-	     				notUnderstood(res);
-	     			});	
-	     		}
-	     		
-	     	} else {
-	     		console.log(`  No intent matched.`);
-		      	notUnderstood(res);
-	     	}
+	      if (!error) {			        
+	        analyzeForIntentV1(body, res);
  
 	      } else {
 	        console.error("e:"+error);
@@ -75,6 +107,7 @@ module.exports = function(app, key) {
 	      }
 	    });
 	});
+
 
 
 }
