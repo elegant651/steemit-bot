143e66823aa31a529775bddc958482973d8eb5c3
elegant651 <korea651@gmail.com>
2018-11-17 22:15:41 +0900

    response test

--- nugu.js
+++ nugu.js
@@ -2,11 +2,85 @@ module.exports = function(app, key) {
 
 	const request = require('request');
 
+	const notUnderstood = (res) => {		
+		const msg = '죄송해요. 알아듣지 못했어요. 다시 말해줄래요?';		
+		res.json({
+        	"version": "2.0",
+        	"resultCode": "OK",
+        	"output": {
+          		"content": msg
+          	}
+        })
+	}
+
+	const actionOnViewIntent = (discuss_option, res) => {
+		const st_query = {"tag": "kr", "limit": 10};
+ 		if(discuss_option=='최신'){
+ 			app.getDiscussionsByCreated(st_query).then((result) => {
+					let msg = "";
+					result.forEach((entry) => {
+						const url = "https://steemit.com/"+entry.url;
+						msg += entry.title +"\n URL: "+url;
+					});
+					
+					res.json({ 'speech': msg, 'displayText': msg });
+ 			}).catch((err) => {
+ 				notUnderstood(res);
+ 			});
+ 		} else if(discuss_option=='핫한'){	     			
+ 			app.getDiscussionsByHot(st_query).then((result) => {	     				
+				let msg = "";
+				result.forEach((entry) => {
+					const url = "https://steemit.com/"+entry.url;
+					msg += entry.title +"\n URL: "+url;
+				});
+				
+				res.json({ 'speech': msg, 'displayText': msg });	     					     		
+ 			}).catch((err) => {
+ 				notUnderstood(res);
+ 			});	
+ 		}
+	}
+
+	const actionOnVoteIntent = (tag, res) => {		
+		app.getDiscussionsByTag(tag).then((post) => {
+			console.log(JSON.stringify(post));
+
+			app.vote(post, 100).then((post) => {
+				app.comment(tag, post).then((post) => {		
+				
+					let msg = `${tag}의 자동 보팅이 완료되었습니다! `;
+					const url = "https://steemit.com/"+post.url;
+					msg += post.title +"\n URL: "+url;							
+
+					res.json({ 'speech': msg, 'displayText': msg });			
+				}).catch((err) => {
+					console.error(err);
+					const errorMsg = `다음과 같은 이유로, 프로세스가 완료되지 못했습니다 : ${err}`;
+					res.json({ 'speech': errorMsg, 'displayText': errorMsg});
+				});
+			}).catch((err) => {
+				console.error(err);
+				const errorMsg = `다음과 같은 이유로, 프로세스가 완료되지 못했습니다 : ${err}`;
+				res.json({ 'speech': errorMsg, 'displayText': errorMsg});
+			});
+		}).catch((err) => {
+			console.error(err);
+			notUnderstood(res);
+		});						
+	}
+
 	app.post('/answer.view_intent', (req, res) => {
 		const body = req.body;	
 
 		console.log(JSON.stringify(body));
+		notUnderstood(res);
+		// if(!body){
+		//   notUnderstood(res);
+	 //  	  return;
+	 //  	}
 
+		// analyzeForIntentV1(body, res);
 	})
 
 	app.post('/answer.vote_intent', (req, res) => {
